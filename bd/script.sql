a4e760de78a99dfa2bc161f5c0c41569e3b39558
Rafael Sandim <rafael.sandim@before.com.br>
2016-04-29 09:13:28 -0400

    update de banco

--- bd/script.sql
+++ bd/script.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: 18-Abr-2016 às 20:16
+-- Generation Time: 29-Abr-2016 às 09:12
 -- Versão do servidor: 5.5.42
 -- PHP Version: 7.0.0
 
@@ -26,15 +26,16 @@ DROP TABLE IF EXISTS `clientes`;
 CREATE TABLE `clientes` (
   `id` int(11) NOT NULL,
   `nome` varchar(40) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `clientes`
 --
 
 INSERT INTO `clientes` (`id`, `nome`) VALUES
-(1, 'Joaozinho'),
-(2, 'Mateus');
+(27, ''),
+(28, 'ewqew'),
+(29, 'eqwew');
 
 -- --------------------------------------------------------
 
@@ -48,17 +49,41 @@ CREATE TABLE `compras` (
   `valor_total` decimal(10,0) NOT NULL,
   `created` datetime NOT NULL,
   `vendedor_id` int(11) NOT NULL,
-  `cliente_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `cliente_id` int(11) NOT NULL,
+  `imagem` varchar(255) NOT NULL,
+  `imagem_dir` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `compras`
 --
 
-INSERT INTO `compras` (`id`, `valor_total`, `created`, `vendedor_id`, `cliente_id`) VALUES
-(1, '1111', '0000-00-00 00:00:00', 2, 2),
-(2, '110', '2016-04-14 02:15:57', 1, 1),
-(3, '0', '2016-04-19 00:09:45', 2, 2);
+INSERT INTO `compras` (`id`, `valor_total`, `created`, `vendedor_id`, `cliente_id`, `imagem`, `imagem_dir`) VALUES
+(8, '0', '2016-04-27 04:02:17', 1, 1, '9.jpeg', '044634ba-031c-48a4-a311-6686bfabad9a'),
+(9, '0', '2016-04-27 04:11:44', 1, 1, '1.jpg', '0e719453-d1c8-4d68-8622-176abb2a7421'),
+(10, '0', '2016-04-28 01:21:53', 1, 27, '2.jpg', '1e0fb5bc-eb2e-4611-96c0-ac36254582a9');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `senha`) VALUES
+(5, 'rjsandim@gmail.com', '$2y$10$hRyVIrsl.9dO.SvwsveNWunz6hojiVhSI6beK5GxPwiUOFy0wcA/K'),
+(6, 'teste', '$2y$10$hRyVIrsl.9dO.SvwsveNWunz6hojiVhSI6beK5GxPwiUOFy0wcA/K'),
+(7, 'a@a.com', '$2y$10$CMhZCz5UESvwoZbeN3mpUuNGSx/.Z/6Ss6P/RBGvvDlvRRW4obudm');
 
 -- --------------------------------------------------------
 
@@ -97,6 +122,12 @@ ALTER TABLE `compras`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `vendedores`
 --
 ALTER TABLE `vendedores`
@@ -110,12 +141,17 @@ ALTER TABLE `vendedores`
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `vendedores`
 --
